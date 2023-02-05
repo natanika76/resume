19cc37d314e78ae1e38f6b3850101ce2018eb231
natanika76 <123064500+natanika76@users.noreply.github.com>
2023-02-05 19:54:16 +0000

    new components resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  nowdate: Date = new Date();
+  books: Array<any> = [
+    {author:'Э.М.Ремарк', booktitle:'Три товарища'},
+    {author:'С.Цвейг', booktitle:'Мария Стюарт'},
+    {author:'М.Булгаков', booktitle:'Собачье сердце'},
+    {author:'Э.Золя', booktitle:'Нана'},
+    {author:'Р.Л.Стивенсон', booktitle:'Черная стрела'}
+  ];
 }
